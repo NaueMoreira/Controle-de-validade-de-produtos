9098264a8a58432257029062880b372da4107d0b
naueM2005 <nauemoreira@gmail.com>
2026-04-20 23:09:59 -0300

    Subindo Projeto
    Subindo projeto

---
+++ farmacia-sistema/backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmacia');
+
+const Produto = mongoose.model('Produto', {
+  codigo: String,
+  nome: String,
+  validade: String,
+  quantidade: Number,
+  sessao: String
+});
+
+app.post('/produtos', async (req, res) => {
+  const produto = new Produto(req.body);
+  await produto.save();
+  res.json(produto);
+});
+
+app.get('/produtos', async (req, res) => {
+  const produtos = await Produto.find();
+  res.json(produtos);
+});
+
+app.get('/produtos/:id', async (req, res) => {
+  const produto = await Produto.findById(req.params.id);
+  res.json(produto);
+});
+
+app.put('/produtos/:id', async (req, res) => {
+  const produto = await Produto.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(produto);
+});
+
+app.delete('/produtos/:id', async (req, res) => {
+  await Produto.findByIdAndDelete(req.params.id);
+  res.json({ ok: true });
+});
+
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
